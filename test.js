e015b6cf5bcefb2c0f5a260bb58ef41dd978b8dc
farhan8728 <farhanmasuk.cse@gmail.com>
2022-09-18 17:12:22 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+let a = [[1,2,3],[1,2,3]];
+let sum = 0;
+    for (let i = 0; i < a.length; i++) {
+      for (let j = 0; j < a[i].length; j++) {
+        sum += a[i][j];
+      }
+    }
+
+ 
+
+  
+  console.log(sum);
+  
